6b998c3bccd1255272b194020317d1039715ce0c
Zach Kanzler <they4kman@gmail.com>
2025-02-13 05:35:23 -0500

    Version 0.1.3: support whitespace-affirming + modifier

--- src/parser.c
+++ src/parser.c
@@ -42173,7 +42173,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_jinja(void) {
     .metadata = {
       .major_version = 0,
       .minor_version = 1,
-      .patch_version = 2,
+      .patch_version = 3,
     },
   };
   return &language;
